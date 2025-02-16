3d020833fa432cc074eebd278abe516d5676f16a
Siddhesh Shinde <siddheshshinde42556@gmail.com>
2025-02-16 15:25:37 +0530

    onhover and onleave

--- 1.js
+++ 1.js
@@ -1,2 +1,6 @@
-let qu = document.querySelector(".booxx");
-console.log(qu);
+let qu1 = document.querySelector(".booxx1");
+let qu2 = document.querySelector(".booxx2");
+let qu3 = document.querySelector(".booxx3");
+let qu4 = document.querySelector(".booxx4");
+let qu5 = document.querySelector(".booxx5");
+let qu6 = document.querySelector(".booxx6");
